86d1d8fbdcbfd5a73b3ad90b9341d8f8a8a0cfa5
elkarapetian <elkarapetian@yandex-team.ru>
2025-03-30 23:44:21 +0300

    FIN-39: add signup button

--- finess/finess/Scenes/SignUp/SignUpViewController.swift
+++ finess/finess/Scenes/SignUp/SignUpViewController.swift
@@ -20,20 +20,49 @@ class SignUpViewController: UIViewController {
         return label
     }()
 
+    private let signupButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Зарегистрироваться", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 17, weight: .medium)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = button.state == .normal ? UIColor(named: "ActiveButton"): UIColor(named: "DisabledButton")
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 26
+        return button
+    }()
+
     private let passwordTextField = RegisterTextField(placeholder: "Пароль")
 
     private let repeatPasswordTextField = RegisterTextField(placeholder: "Повторите пароль")
+
+    private var buttonBottomConstraint: NSLayoutConstraint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.tapGesture))
         view.addGestureRecognizer(tapGesture)
+        NotificationCenter.default.addObserver(self, selector: #selector(SignUpViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(SignUpViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         passwordTextField.becomeFirstResponder()
+    }
+
+    @objc func keyboardWillShow(notification: Notification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            buttonBottomConstraint = signupButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -18 - keyboardSize.height)
+            buttonBottomConstraint.isActive = true
+            view.layoutIfNeeded()
+        }
+    }
+
+    @objc func keyboardWillHide(notification: Notification) {
+        buttonBottomConstraint = signupButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -18)
+        buttonBottomConstraint.isActive = true
+        view.layoutIfNeeded()
     }
 
     private func setupUI() {
@@ -48,6 +77,7 @@ class SignUpViewController: UIViewController {
         view.addSubview(titleLabel)
         view.addSubview(passwordTextField)
         view.addSubview(repeatPasswordTextField)
+        view.addSubview(signupButton)
 
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
@@ -62,8 +92,14 @@ class SignUpViewController: UIViewController {
             repeatPasswordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             repeatPasswordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             repeatPasswordTextField.heightAnchor.constraint(equalToConstant: 56),
+
+            signupButton.heightAnchor.constraint(equalToConstant: 48),
+            signupButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            signupButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
         ])
-        view.addSubview(titleLabel)
+
+        buttonBottomConstraint = signupButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -18)
+        buttonBottomConstraint.isActive = true
     }
 
     // MARK: - Actions
